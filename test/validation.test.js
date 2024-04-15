acdacb7a4cd16c065592c7452cb00db8abe492fe
Ross Tauchert <57552771+rdtauche@users.noreply.github.com>
2024-04-15 15:26:10 -0400

    added more tests

--- test/validation.test.js
+++ test/validation.test.js
@@ -9,6 +9,35 @@ describe("Shapes Class", () => {
       const logo = new LogoDetails('RDT', 'black', 'circle', 'blue');
 
       expect(logo.text).toEqual('RDT');
+
+    });
+  });
+});
+
+describe("Shapes Class", () => {
+  // TODO: Write a test that checks if isPassword() returns false when the password is less than 8 characters.
+
+  describe("New instance of shape", () => {
+    // TODO: Write a test that checks if isPassword() returns false when the password is less than 8 characters.
+    it("should generate a new instance of shape", () => {
+      const logo = new LogoDetails('RDT', 'red', 'circle', 'white');
+
+      expect(logo.shapeColor).toEqual('white');
+
+    });
+  });
+});
+
+describe("Shapes Class", () => {
+  // TODO: Write a test that checks if isPassword() returns false when the password is less than 8 characters.
+
+  describe("New instance of shape", () => {
+    // TODO: Write a test that checks if isPassword() returns false when the password is less than 8 characters.
+    it("should generate a new instance of shape", () => {
+      const logo = new LogoDetails('RDT', 'red', 'circle', 'white');
+
+      expect(logo.shape).toEqual('circle');
+
     });
   });
 });
